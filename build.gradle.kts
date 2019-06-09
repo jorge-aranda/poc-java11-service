29be2548465d1bf56e5dc6220333d37e898201b5
Jorge Aranda de Miguel <jorgearanda123@gmail.com>
2019-06-09 17:18:19 +0200

    refactor: Using spaces instead of tabs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
+
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.asciidoctor.convert") version "1.5.3"
-	id("org.springframework.boot") version "2.1.5.RELEASE"
-	id("io.spring.dependency-management") version "1.0.7.RELEASE"
-	kotlin("jvm") version "1.2.71"
-	kotlin("plugin.spring") version "1.2.71"
+    id("org.asciidoctor.convert") version "1.5.3"
+    id("org.springframework.boot") version "2.1.5.RELEASE"
+    id("io.spring.dependency-management") version "1.0.7.RELEASE"
+    kotlin("jvm") version "1.2.71"
+    kotlin("plugin.spring") version "1.2.71"
 }
 
 group = "es.jaranda.poc"
@@ -13,7 +14,7 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 extra["snippetsDir"] = file("build/generated-snippets")
@@ -21,39 +22,39 @@ extra["springBootAdminVersion"] = "2.1.5"
 extra["springCloudVersion"] = "Greenwich.SR1"
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-hateoas")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("de.codecentric:spring-boot-admin-starter-client")
-	implementation("de.codecentric:spring-boot-admin-starter-server")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.cloud:spring-cloud-starter-contract-verifier")
-	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-hateoas")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("de.codecentric:spring-boot-admin-starter-client")
+    implementation("de.codecentric:spring-boot-admin-starter-server")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.cloud:spring-cloud-starter-contract-verifier")
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
 }
 
 dependencyManagement {
-	imports {
-		mavenBom("de.codecentric:spring-boot-admin-dependencies:${property("springBootAdminVersion")}")
-		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
-	}
+    imports {
+        mavenBom("de.codecentric:spring-boot-admin-dependencies:${property("springBootAdminVersion")}")
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "1.8"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "1.8"
+    }
 }
 
 tasks.test {
-	outputs.dir(snippetsDir)
+    outputs.dir(snippetsDir)
 }
 
 tasks.asciidoctor {
-	inputs.dir(snippetsDir)
-	dependsOn(test)
+    inputs.dir(snippetsDir)
+    dependsOn(test)
 }
--- src/main/kotlin/es/jaranda/poc/java11/service/PocJava11ServiceApplication.kt
+++ src/main/kotlin/es/jaranda/poc/java11/service/PocJava11ServiceApplication.kt
@@ -1,3 +1,4 @@
+
 package es.jaranda.poc.java11.service
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -7,5 +8,5 @@ import org.springframework.boot.runApplication
 class PocJava11ServiceApplication
 
 fun main(args: Array<String>) {
-	runApplication<PocJava11ServiceApplication>(*args)
+    runApplication<PocJava11ServiceApplication>(*args)
 }
