f0835383d240ba719ab2575f812aa6b869c19404
Jorge Aranda de Miguel <jorgearanda123@gmail.com>
2019-06-10 03:11:33 +0200

    Upgrading to 0.1.1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "es.jaranda.poc"
-version = "0.1.0.RELEASE"
+version = "0.1.1-SNAPSHOT"
 description = "PoC Java 11 Service made with Kotlin and Spring Boot"
 
 java.sourceCompatibility = JavaVersion.VERSION_11
